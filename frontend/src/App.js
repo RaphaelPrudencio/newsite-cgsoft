9f48e6a319173dc27018e3402f863059afb28203
Raphael Prudencio <raphaelprudencio@hotmail.com>
2025-09-24 16:49:29 -0400

    suavizando a troca de rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-// src/App.js
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { Toaster } from "./components/ui/toaster";
+
+// Framer Motion
+import { AnimatePresence } from "framer-motion";
+import PageTransition from "./components/Transitions/PageTransition";
+import ScrollToTop from "./components/Transitions/ScrollToTop";
 
 // Layout Components
 import Header from "./components/Layout/Header";
@@ -14,7 +18,61 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
-import SearchResults from "./pages/SearchResults"; // ⬅ novo
+import SearchResults from "./pages/SearchResults"; // resultados da busca
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <PageTransition>
+                <Home />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/sobre"
+            element={
+              <PageTransition>
+                <About />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/servicos"
+            element={
+              <PageTransition>
+                <Services />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/contato"
+            element={
+              <PageTransition>
+                <Contact />
+              </PageTransition>
+            }
+          />
+          <Route
+            path="/buscar"
+            element={
+              <PageTransition>
+                <SearchResults />
+              </PageTransition>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
+    </>
+  );
+}
 
 function App() {
   return (
@@ -23,13 +81,7 @@ function App() {
         <BrowserRouter>
           <Header />
           <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/sobre" element={<About />} />
-              <Route path="/servicos" element={<Services />} />
-              <Route path="/contato" element={<Contact />} />
-              <Route path="/buscar" element={<SearchResults />} /> {/* ⬅ novo */}
-            </Routes>
+            <AnimatedRoutes />
           </main>
           <Footer />
         </BrowserRouter>
